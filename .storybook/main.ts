2565774aaa94ce9a1bbd44b79433537aeb576aef
ytw <a9600125a@gmail.com>
2023-05-22 15:56:14 +0800

    feat: install and setup @storybook/addon-storysource

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,6 @@
+import path from "path";
 import type { StorybookConfig } from "@storybook/react-vite";
+
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
@@ -9,6 +11,18 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    {
+      name: "@storybook/addon-storysource",
+      options: {
+        rule: {
+          // test: [/\.stories\.jsx?$/], This is default
+          include: [path.resolve(__dirname, "../packages/components")], // You can specify directories
+        },
+        loaderOptions: {
+          prettierConfig: { printWidth: 80, singleQuote: false },
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-vite",
