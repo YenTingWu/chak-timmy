b4f898fd203a0c2253f5496ddd4a06e264f5e1a0
ytw <a9600125a@gmail.com>
2023-05-31 14:00:50 +0800

    feat(DateRangePicker): make the component allow external props passing in and to be controlled

--- packages/components/date-range-picker/src/date-range-picker.tsx
+++ packages/components/date-range-picker/src/date-range-picker.tsx
@@ -2,17 +2,29 @@ import React from "react";
 import { DateRangePickerProvider } from "./date-range-picker.provider";
 import { useDateRangePicker } from "@react-aria/datepicker";
 import { useDateRangePickerState } from "@react-stately/datepicker";
-import type { DateValue } from "@react-aria/datepicker";
-import type { DateRangePickerStateOptions } from "@react-stately/datepicker";
+import { CalendarDate, parseDate } from "@internationalized/date";
 
-interface DateRangPicker extends DateRangePickerStateOptions<DateValue> {
+export interface DateRangePickerProps {
+  isOpen?: boolean;
+  defaultOpen?: boolean;
+  onOpenChange?: (isOpen: boolean) => void;
+  shouldCloseOnSelect?: boolean | (() => boolean);
+  value?: { start: string; end: string };
+  defaultValue?: { start: string; end: string };
+  onChange?: (value: { start: string; end: string }) => void;
   children: React.ReactNode;
 }
 
-export const DateRangePicker = ({ children, ...props }: DateRangPicker) => {
+export const DateRangePicker = ({
+  children,
+  ...props
+}: DateRangePickerProps) => {
   const ref = React.useRef<HTMLDivElement>(null);
-  const state = useDateRangePickerState(props);
-  const elementProps = useDateRangePicker(props, state, ref);
+
+  const newProps = useDateRangePickerProps(props);
+
+  const state = useDateRangePickerState(newProps);
+  const elementProps = useDateRangePicker(newProps, state, ref);
 
   return (
     <DateRangePickerProvider
@@ -26,3 +38,56 @@ export const DateRangePicker = ({ children, ...props }: DateRangPicker) => {
     </DateRangePickerProvider>
   );
 };
+
+function useDateRangePickerProps(
+  props: Omit<DateRangePickerProps, "children"> = {},
+) {
+  const {
+    value: _value,
+    defaultValue: _defaultValue,
+    onChange: _onChange,
+    ...rest
+  } = props;
+
+  const value = React.useMemo(
+    () =>
+      typeof _value !== "undefined"
+        ? { start: parseDate(_value.start), end: parseDate(_value.end) }
+        : undefined,
+    [_value],
+  );
+
+  const defaultValue = React.useMemo(
+    () =>
+      typeof _defaultValue !== "undefined"
+        ? {
+            start: parseDate(_defaultValue.start),
+            end: parseDate(_defaultValue.end),
+          }
+        : undefined,
+    [_defaultValue],
+  );
+
+  const onChange = React.useMemo(
+    () => (_onChange ? convertOnChangeCallback(_onChange) : undefined),
+    [_onChange],
+  );
+
+  return {
+    onChange,
+    value,
+    defaultValue,
+    ...rest,
+  };
+}
+
+function convertOnChangeCallback(
+  func: (value: { start: string; end: string }) => void,
+) {
+  return (value: { start: CalendarDate; end: CalendarDate }) => {
+    const start = value.start.toString();
+    const end = value.end.toString();
+
+    func({ start, end });
+  };
+}
