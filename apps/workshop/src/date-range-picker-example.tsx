a316660b36c0a15d7a7a5ea0e9381317f0816b1f
ytw <a9600125a@gmail.com>
2023-05-31 14:01:23 +0800

    feat(workspace): update DateRangePicker example

--- apps/workshop/src/date-range-picker-example.tsx
+++ apps/workshop/src/date-range-picker-example.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { chakra } from "@chakra-ui/system";
 import {
   DateRangePicker,
@@ -14,31 +15,50 @@ import {
   DateRangePickerCalendarNextButton,
   DateRangePickerCalendarPrevButton,
 } from "date-range-picker";
+import { getISODateString } from "@chak-timmy/utils";
 
 export const DateRangePickerExample = () => {
+  const [value, setValue] = React.useState({
+    start: getISODateString(new Date()),
+    end: getISODateString(new Date()),
+  });
+
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+
   return (
-    <DateRangePicker>
-      <DateRangePickerGroup mt="36px">
-        <DateRangePickerStartField />
-        <chakra.div mx="16px">-</chakra.div>
-        <DateRangePickerEndField />
-      </DateRangePickerGroup>
-      <DateRangePickerPopover>
-        <DateRangePickerDialog>
-          <DateRangePickerCalendar>
-            <DateRangePickerCalendarContent>
-              <DateRangePickerCalendarHeader>
-                <DateRangePickerCalendarTitle />
-                <chakra.div display="flex" alignItems="center">
-                  <DateRangePickerCalendarPrevButton />
-                  <DateRangePickerCalendarNextButton />
-                </chakra.div>
-              </DateRangePickerCalendarHeader>
-              <DateRangePickerCalendarGrid />
-            </DateRangePickerCalendarContent>
-          </DateRangePickerCalendar>
-        </DateRangePickerDialog>
-      </DateRangePickerPopover>
-    </DateRangePicker>
+    <>
+      <chakra.h1>Date Now</chakra.h1>
+      <chakra.h3>{`${value.start} --> ${value.end}`}</chakra.h3>
+      <DateRangePicker
+        value={value}
+        isOpen={isOpen}
+        onOpenChange={setIsOpen}
+        onChange={(newValue: { start: string; end: string }) =>
+          setValue(newValue)
+        }
+      >
+        <DateRangePickerGroup mt="36px">
+          <DateRangePickerStartField />
+          <chakra.div mx="16px">-</chakra.div>
+          <DateRangePickerEndField />
+        </DateRangePickerGroup>
+        <DateRangePickerPopover>
+          <DateRangePickerDialog>
+            <DateRangePickerCalendar>
+              <DateRangePickerCalendarContent>
+                <DateRangePickerCalendarHeader>
+                  <DateRangePickerCalendarTitle />
+                  <chakra.div display="flex" alignItems="center">
+                    <DateRangePickerCalendarPrevButton />
+                    <DateRangePickerCalendarNextButton />
+                  </chakra.div>
+                </DateRangePickerCalendarHeader>
+                <DateRangePickerCalendarGrid />
+              </DateRangePickerCalendarContent>
+            </DateRangePickerCalendar>
+          </DateRangePickerDialog>
+        </DateRangePickerPopover>
+      </DateRangePicker>
+    </>
   );
 };
