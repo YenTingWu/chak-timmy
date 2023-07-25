0d2ef861a5fd73143fbb917b0d3a11429351f4ab
ytw <a9600125a@gmail.com>
2023-07-24 17:44:10 -0700

    chore: setup jest with swc to test React components

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,25 @@ module.exports = {
   collectCoverageFrom: ["packages/**/*.{ts,tsx}"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest",
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      /** https://swc.rs/docs/configuration/compilation#jsctransformreact */
+      {
+        sourceMaps: true,
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+            decorators: false,
+            dynamicImport: false,
+          },
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+      },
+    ],
   },
 };
