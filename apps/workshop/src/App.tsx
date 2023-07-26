7be7854bcda208971658e227d6e01b94f680ebf6
ytw <a9600125a@gmail.com>
2023-07-25 19:02:12 -0700

    chore: install chakra-ui in workshop

--- apps/workshop/src/App.tsx
+++ apps/workshop/src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
+import { VStack } from "@chakra-ui/react";
 import { DatePickerExample } from "./date-picker-example";
 import { DateRangePickerExample } from "./date-range-picker-example";
 
 function App() {
   return (
-    <div>
+    <VStack
+      minH="100vh"
+      spacing="10"
+      alignItems="center"
+      justifyContent="center"
+    >
       <DateRangePickerExample />
       <DatePickerExample />
-    </div>
+    </VStack>
   );
 }
 
